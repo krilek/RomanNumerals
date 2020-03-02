df53319676ea34f19f25adcb338162720c9fbe36
krilek <krilek@gmail.com>
2020-03-02 20:47:43 +0100

    .............................................................................................

--- src/RomanNumeral.java
+++ src/RomanNumeral.java
@@ -4,6 +4,7 @@ public class RomanNumeral {
     private final static TreeMap<Integer, String> map = new TreeMap<>();
 
     static {
+        map.put(90, "XC");
         map.put(50, "L");
         map.put(40, "XL");
         map.put(10, "X");

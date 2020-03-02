9eb107d549dd1895c74b0cf257ad03fda7a265c3
krilek <krilek@gmail.com>
2020-03-02 20:14:17 +0100

    Added base project.

---
+++ src/RomanNumeral.java
@@ -0,0 +1,10 @@
+public class RomanNumeral {
+    private final int arabicNumber;
+    RomanNumeral(int arabicNumber){
+        this.arabicNumber = arabicNumber;
+    }
+
+    public String getRomanNumeral() {
+        return null;
+    }
+}
